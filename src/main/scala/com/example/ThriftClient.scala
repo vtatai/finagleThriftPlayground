0958469ca1b5a60aec57737a23ac2f237b448dd3
Victor Tatai <vtatai@fitbit.com>
2016-05-18 08:58:54 -0700

    Adding filter to client

--- src/main/scala/com/example/ThriftClient.scala
+++ src/main/scala/com/example/ThriftClient.scala
@@ -1,14 +1,20 @@
 package com.example
 
-import com.twitter.finagle.Thrift
+import com.twitter.finagle.{Service, SimpleFilter, Thrift}
 import com.twitter.finagle.example.thriftscala.Hello
-import com.twitter.finagle.thrift.ClientId
-import com.twitter.util.Await
+import com.twitter.finagle.thrift.{ClientId, ThriftClientRequest}
+import com.twitter.util.{Await, Future}
 
 object ThriftClient {
   def main(args: Array[String]) {
     val client = Thrift.client
       .withClientId(ClientId("CLIENT ID HARD TO MISS"))
+      .filtered(new SimpleFilter[ThriftClientRequest, Array[Byte]] {
+        override def apply(request: ThriftClientRequest, service: Service[ThriftClientRequest, Array[Byte]]): Future[Array[Byte]] = {
+          println("Filtered")
+          service(request)
+        }
+      })
       .newIface[Hello.FutureIface]("localhost:8081", "SPECIAL LABEL HARD TO MISS")
     println(Await.result(client.hi()))
   }
